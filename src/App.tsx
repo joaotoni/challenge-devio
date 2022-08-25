a07db8f5edf2a6fc541df38bd415e9474e12169c
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-08-24 23:15:49 -0300

    refact: removing de imports in the App, becaus is not more necessary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
-import Header from "./components/Header"
-import Modal from "./components/Modal"
-import Kitchen from "./pages/Kitchen"
-import Home from "./pages/Home"
-import Payment from "./pages/Payment"
-import "./styles/global.css"
-import Withdrawal from "./pages/Withdrawal"
 import Routes from "./Routes"
 
 export default function App() {
